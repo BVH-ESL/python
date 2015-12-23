b152b79c8525b88c368c1086da52a5cf6a447d65
BVH-ESL <batman12922@gmail.com>
2015-12-23 18:47:44 +0700

    add serial controll motor

--- serial_drive_motor/keyboard_controll.py
+++ serial_drive_motor/keyboard_controll.py
@@ -2,10 +2,10 @@ import serial
 from time import sleep
 import tty, sys
 
-speed_a = 0
-speed_b = 0
-dir_a = 1
-dir_b = 1
+speed_a = 50
+speed_b = 50
+dir_a = 0
+dir_b = 0
 
 drive_command = "("+str(speed_a*dir_a)+","+str(speed_b*dir_b)+"):"
 
@@ -27,7 +27,21 @@ while 1:
 	ch = sys.stdin.read(1)
 	if ch == 'e':
 		break
-	else if ch == 'w':
+	elif ch == 'w':
+		dir_a = 1
+		dir_b = 1
+	elif ch == 's':
+		dir_a =-1
+		dir_b = -1
+	elif ch == 'a':
+		dir_a = -1
+		dir_b = 1
+	elif ch == 'd':
+		dir_a = 1
+		dir_b = -1
+	elif ch == 'x':
+		dir_a = 0
+		dir_b = 0
 		
 	drive_command = "("+str(speed_a*dir_a)+","+str(speed_b*dir_b)+"):"
 	ser.write(drive_command)
